852cdc962d14a86428dab1a7d969330b75777d3a
Roberto “ResaloliPT” Pires <roberto_pires97@sapo.pt>
2021-04-03 21:32:23 +0000

    Started menus

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartGame(GameObject gameObject)
+    {
+
+    }
+
+    public void OnShowBindings(GameObject gameObject)
+    {
+
+    }
+
+    public void OnExitBindings(GameObject gameObject)
+    {
+
+    }
+}

c23354845b84440512291d1be6409f55347eb284
Nuno Amaral <n20amaral@outlook.com>
2021-04-03 17:41:14 +0000

    added tide object generation

--- Assets/Scripts/Tide/TideController.cs
+++ Assets/Scripts/Tide/TideController.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class TideController : MonoBehaviour
 {
     public float waitTime;
-    public float timer = 0.0f;
-    public bool MovingRight;
+    float timer = 0.0f;
+    public bool MovingRight { get; set; }
+
+    public GameObject netObj;
+    public MeshCollider background;
 
     // Start is called before the first frame update
     void Start()
@@ -21,8 +24,15 @@ public class TideController : MonoBehaviour
         {
             MovingRight = !MovingRight;
             timer -= waitTime;
+            Instantiate(netObj, GetRandomLocation(), new Quaternion());
         }
-            
-       
+    }
+
+    public Vector3 GetRandomLocation()
+    {
+        var x = Random.Range(background.bounds.min.x, background.bounds.max.x);
+        var y = Random.Range(background.bounds.min.y, background.bounds.max.y);
+
+        return new Vector3(x, y, 0f);
     }
 }
